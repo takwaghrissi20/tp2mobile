d5d71773601213c6a1c85a3a13f216ea372457d8
PROG2-P3 <PROG2-P3-prog2@enetcom.usf.tn>
2024-10-08 15:09:05 +0100

    finaal tp2

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hellotoast;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int mCount = 0;
+    private TextView mShowCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
     }
 }
